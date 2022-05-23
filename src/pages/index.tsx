569dd07f622415bf124a2c4543168cf7f071fae2
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-05-23 16:14:49 -0300

    feat(home): created home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { GetStaticProps } from 'next';
-
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +27,60 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return (
+    <main className={styles.mainContent}>
+      <div className={commonStyles.container}>
+        <header>
+          <img src="/logo.svg" alt="logo" />
+        </header>
+        <section>
+          <ul>
+            {postsPagination.results.map(post => (
+              <li key={post.uid}>
+                <Link href={`/post/${post.uid}`}>
+                  <a>
+                    <strong>{post.data.title}</strong>
+                    <p>{post.data.subtitle}</p>
+                    <div>
+                      <time>
+                        <FiCalendar size={15} />
+                        {format(
+                          new Date(post.first_publication_date),
+                          'dd MMM yyyy',
+                          { locale: ptBR }
+                        )}
+                      </time>
+                      <p>
+                        <FiUser size={15} />
+                        {post.data.author}
+                      </p>
+                    </div>
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+        {postsPagination.next_page && (
+          <footer>
+            <button type="button">Carregar mais posts</button>
+          </footer>
+        )}
+      </div>
+    </main>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
-
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts');
+  return {
+    props: {
+      postsPagination: {
+        results: postsResponse.results,
+        next_page: postsResponse.next_page,
+      },
+    },
+  };
+};
